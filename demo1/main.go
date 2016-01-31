de42dd883dfef934fc3442f6b278a1d98b70624b
liuwencai <821272852@qq.com>
2016-01-31 09:30:44 +0800

    go study

---
+++ demo1/main.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	var a int
+	var b bool
+	var c string
+	var d *int
+	var f [1]int
+	fmt.Println(a)
+
+	fmt.Println(b)
+
+	fmt.Println(c)
+
+	fmt.Println(d)
+	fmt.Println(f)
+}
+
