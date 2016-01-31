d7afa9b3f26a8e2410fe4f71bb51e3c09d05ad03
liuwencai <821272852@qq.com>
2016-01-31 09:52:04 +0800

    Chinese type

--- demo1/main.go
+++ demo1/main.go
@@ -12,13 +12,15 @@ func main(){
 	var f [1]int
 	type Int int32//bool,string
 	var g Int
-
+	type 文本 string
+	var h 文本="hello world"
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
 	fmt.Println(d)
 	fmt.Println(f)
 	fmt.Println(g)
+	fmt.Println(h)
 
 }
 
