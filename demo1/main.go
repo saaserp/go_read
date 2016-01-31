cf22af86a9f8bb7bc6dcb22beefcc3f50f933428
liuwencai <821272852@qq.com>
2016-01-31 09:44:38 +0800

    nil

--- demo1/main.go
+++ demo1/main.go
@@ -10,13 +10,14 @@ func main(){
 	var c string
 	var d *int
 	var f [1]int
+	type Int int32
+	var g Int
 	fmt.Println(a)
-
 	fmt.Println(b)
-
 	fmt.Println(c)
-
 	fmt.Println(d)
 	fmt.Println(f)
+	fmt.Println(g)
+
 }
 
