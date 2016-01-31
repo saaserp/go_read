5f64034d185f47238858102ade3a1c418108d075
liuwencai <821272852@qq.com>
2016-01-31 10:27:29 +0800

    _

--- demo2/main.go
+++ demo2/main.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 var a int
+var b int =1
+var (
+	d int=1
+	e bool=true
+	f string="hello"
+)
 func main(){
+	a = 1
+	c :=1
 	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d,e,f)
 }
